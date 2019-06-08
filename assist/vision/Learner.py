8aec6b2caafe95672afbfc65eb8414fbcb76c584
Aniket Maurya <aniketmaurya@outlook.com>
2019-06-09 04:24:23 +0530

    "module update"

--- assist/vision/Learner.py
+++ assist/vision/Learner.py
@@ -1,3 +1,15 @@
+from .utils import subfolder_count
+from .imagedatabunch import *
+
+from pathlib import Path
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+import matplotlib.pyplot as plt
+import tensorflow.keras.applications as models
+from tensorflow.keras import backend as K
+import scipy
+
 class Learner:
 
     def __init__(self, data:ImageDataBunch, model:'model name'=models.resnet50.ResNet50):
@@ -81,4 +93,9 @@ class Learner:
     
     
 '''
+def cnn_learner(data:ImageDataBunch, model:'model name'=models.resnet50.ResNet50):
+    "Creates a Learner object"
     
+    learn = Learner(data, model)
+    learn.convNet()
+    return learn
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='tf_assist',   #pip install tf_assist
-      version='0.1.2',
+      version='0.1.3',
       description='Deep Learning library on top of Tensorflow for fast prototyping',
       url='http://github.com/aniketmaurya/tf_assist',
       author='Aniket Maurya',
